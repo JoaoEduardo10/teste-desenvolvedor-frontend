6fa4051d2877a264afd2705ba13e082d69d34a74
João Eduardo <joaoeduardoj920@gmail.com>
2024-04-16 19:16:23 -0300

    feat: adding resposividdae to tablet

--- front-end/src/components/ProductTable/index.tsx
+++ front-end/src/components/ProductTable/index.tsx
@@ -27,7 +27,7 @@ const ProductTable = ({ medicines }: ProductTableProps) => {
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3 phone:text-xl">
+            <th scope="col" className="px-6 py-3 phone:text-sm">
               Nome do medicamento
             </th>
             <th
